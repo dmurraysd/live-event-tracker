3eb312dbbb2749023f388d671352e247c8c5591c
David Murray <david.murray@williamhill.com>
2025-07-04 23:46:26 +0100

    initial commit

---
+++ src/main/java/com/dmurraysd/spring/LiveEventTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dmurraysd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveEventTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveEventTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmurraysd/spring/LiveEventTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmurraysd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveEventTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
