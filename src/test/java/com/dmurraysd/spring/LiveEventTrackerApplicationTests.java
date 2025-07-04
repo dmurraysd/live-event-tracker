c490c71b11c39927c3b0fb0199d13fd31814c4b6
David Murray <david.murray@williamhill.com>
2025-07-05 00:22:41 +0100

    add project structure and add shell integration test

--- src/test/java/com/dmurraysd/spring/LiveEventTrackerApplicationTests.java
+++ src/test/java/com/dmurraysd/spring/LiveEventTrackerApplicationTests.java
@@ -1,13 +1,18 @@
 package com.dmurraysd.spring;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LiveEventTrackerApplicationTests {
 
+    @DisplayName("Consumer sends live update for an event and event update is sent to topic")
     @Test
-    void contextLoads() {
+    void shouldProcessIncomingRequestAndPublishEventUpdateToTopic() {
+
     }
 
 }
